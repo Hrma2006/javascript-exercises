5b4c3120bdcfde9fff656076ad695ea346296dff
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-11 22:48:43 +0300

    done the second test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (repeat, times) {
+	if (times < 0) {
+		return "ERROR";
+	}
+	let result = "";
+	for (times; times > 0; times--) {
+		result += repeat;
+	}
+	return result;
 };
 
 // Do not edit below this line
