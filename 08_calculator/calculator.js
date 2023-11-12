5d67af4be9d8970d7479b0d79a1e92d52de63823
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-12 17:20:10 +0300

    done the eights test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	all=0;
+  for(i=0;i<nums.length;i++){
+    all+=nums[i]
+  }
+  return all
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  all=1;
+  for(i=0;i<nums.length;i++){
+    all*=nums[i]
+  }
+  return all
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+  mult=num1
+	while(num2>1){
+    num1*=mult
+    num2-=1
+  }
+  return num1
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	if(num1==1||num1==0){
+    return 1;
+  }
+  else{
+    return num1*factorial(num1-1)
+  }
 };
 
 // Do not edit below this line
