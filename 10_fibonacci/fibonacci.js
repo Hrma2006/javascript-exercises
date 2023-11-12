207e6f665da31d6004ea44077a00ad4c0c026f78
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-12 20:35:16 +0300

    done the tenth test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n=parseInt(n)
+  if(n<0){
+    return "OOPS"
+  }
+	else if (n == 1||n==0) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);}
 
 };
 
