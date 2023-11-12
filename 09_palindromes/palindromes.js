82f2dc3f266532383280e438f9fa057aefb23c42
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-12 17:39:14 +0300

    done the ninth test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string=string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+  rev=string.split("").reverse().join("").toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+  if(string==rev)return true
+  else return false
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
