821d92e26ce13c132acd0655fc161ab83104b5ee
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-12 16:35:06 +0300

    done the fifth test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (from, to) {
+	let sum = 0;
+	if (typeof from != typeof sum || typeof to != typeof sum) {
+		return "ERROR";
+	}
+	else if (from < 0 || to < 0) {
+		return "ERROR";
+	} else if (from < to) {
+		for (from; from <= to; from++) {
+			sum += from;
+		}
+		return sum;
+	} else if (from > to) {
+		for (to; to <= from; to++) {
+			sum += to;
+		}
+		return sum;
+	} 
 };
 
 // Do not edit below this line
